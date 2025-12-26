c00a1a0d52f9d1ee21681a2e107ad9e91df2471e
MadeByPark <dudwlsdl4817@gmail.com>
2025-12-26 13:58:40 +0900

    수정

--- src/test/java/com/back/domain/together/controller/TogetherControllerTest.java
+++ src/test/java/com/back/domain/together/controller/TogetherControllerTest.java
@@ -16,7 +16,7 @@ import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -36,7 +36,8 @@ public class TogetherControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @SuppressWarnings("removal")
+    @MockBean
     private TogetherService togetherService;
 
     @Test
